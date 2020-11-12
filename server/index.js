0525ea9f1fabd3422b12d8b3cc9f2d728b6408bb
Gledinburgh <gledinburgh@gmail.com>
2020-11-12 16:40:12 -0600

    Created basic express server.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-// express server lives here
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3040;
+
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
