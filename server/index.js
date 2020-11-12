367829354ea034afca478857fa22f670a9b73159
Gledinburgh <gledinburgh@gmail.com>
2020-11-12 16:54:09 -0600

    Fixed typo.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3040;
 
-app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
