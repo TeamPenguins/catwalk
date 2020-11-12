aa74ce23239be5f81498756a83a77f0e30c446dd
Jonathan Garcia <jagspurs7@gmail.com>
2020-11-12 17:34:49 -0600

    created stubbed version of App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      selectedProdId: null,
+    };
+  }
+
+
+
+  render () {
+    return (
+      <div>
+        <div>YourComponentHere</div>
+        <div>YourComponentHere</div>
+        <div>YourComponentHere</div>
+        <div>YourComponentHere</div>
+      </div>
+    );
+  }
+
+}
