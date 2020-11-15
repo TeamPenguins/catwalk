6be3fb17975678b3b6841e69df8439b92d85c4ef
Stephanie Green <steph.green44@gmail.com>
2020-11-15 15:03:36 -0700

    add product overview component structure

---
+++ client/src/OverviewComponents/ProductOverview.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductOverview extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>My Product</h1>
+    );
+  }
+}
+
+export default ProductOverview;
