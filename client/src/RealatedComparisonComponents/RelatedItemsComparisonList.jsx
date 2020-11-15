654c54e12d1ed768d07e3bb62defa6aaae2adc69
Gledinburgh <gledinburgh@gmail.com>
2020-11-14 21:54:15 -0600

    RelatedItemCards now update based on the curret Item selected.

--- client/src/RealatedComparisonComponents/RelatedItemsComparisonList.jsx
+++ client/src/RealatedComparisonComponents/RelatedItemsComparisonList.jsx
@@ -7,28 +7,36 @@ class RelatedItemsAndComparisonList extends React.Component {
     super(props);
     this.state = {
       relatedProductIds: [],
-      selectedProduct: this.props.selectedProduct.id
+      selectedProduct: {}
     };
     this.fetchRelatedProducts = this.fetchRelatedProducts.bind(this);
   }
   fetchRelatedProducts(id) {
-    axios.get(`http://3.21.164.220/products/${this.state.selectedProduct}/related`)
+    axios.get(`http://3.21.164.220/products/${id}/related`)
       .then(data => this.setState({relatedProductIds: data.data}))
-      .catch(console.error('error at fetchRelatedProducts'));
+      .catch(/*console.error('error at fetchRelatedProducts')*/);
+  }
+  //updates the components after selectedProduct has changed in App
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedProduct.id !== this.props.selectedProduct.id ) {
+      this.fetchRelatedProducts(this.props.selectedProduct.id);
+    }
   }
   componentDidMount() {
-    this.fetchRelatedProducts();
+    this.fetchRelatedProducts(this.props.selectedProduct.id);
   }
   render() {
     return (
-
       <div>
         <div className="relatedProductsList">
           <div>related Items</div>
           {
             this.state.relatedProductIds.map(id => {
               return (
-                <RelatedProductCard productChangeMethod={this.props.productChangeMethod} productId={id}/>
+                <RelatedProductCard
+                  key={id}
+                  productChangeMethod={this.props.productChangeMethod}
+                  productId={id}/>
               );
             })
           }
--- client/src/RealatedComparisonComponents/RelatedProductCard.jsx
+++ client/src/RealatedComparisonComponents/RelatedProductCard.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
-/*
-So maybe something like, onClick={() => methodFromApp}
-*/
+import axios from 'axios';
 
-const RelatedProductCard = ({productChangeMethod, selectedProduct}) => {
-  return (
-    <div calssName="productCardCOntainer"
-      onClick={productChangeMethod()}
-    >
+class RelatedProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productInfo: {}
+    };
+    this.fetchProductInfo = this.fetchProductInfo.bind(this);
+  }
 
-    </div>
-  );
-};
+  fetchProductInfo(id) {
+    axios.get(`http://3.21.164.220/products/${id}`)
+      .then(data => this.setState({productInfo: data.data}))
+      .catch(/*console.error('error at fetch product info')*/);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.productId !== this.props.productId) {
+      this.fetchProductInfo(this.props.productId);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchProductInfo(this.props.productId);
+  }
+
+  render() {
+    return (
+      <div
+        calssname="productCardContainer"
+        onClick={() => this.props.productChangeMethod(this.state.productInfo)}>
+        {this.state.productInfo.id}
+      </div>
+    );
+  }
+}
 
 export default RelatedProductCard;
-
-/*
- *
- *
- */
