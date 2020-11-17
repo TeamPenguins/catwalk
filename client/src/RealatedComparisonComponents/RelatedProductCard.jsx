862a1c1f3527a7deadcc196d6e90ba2d43e9eab9
Gledinburgh <gledinburgh@gmail.com>
2020-11-17 10:51:30 -0600

    Related Prodcut cards now use bootstrap card component, and fetch style data.

--- client/src/RealatedComparisonComponents/RelatedProductCard.jsx
+++ client/src/RealatedComparisonComponents/RelatedProductCard.jsx
@@ -1,38 +1,53 @@
 import React from 'react';
 import axios from 'axios';
+import {Card, Column} from 'react-bootstrap';
 
 class RelatedProductCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productInfo: {}
+      productInfo: {},
+      styleInfo: {},
+      noImgFound: 'https://via.placeholder.com/400/700'
     };
     this.fetchProductInfo = this.fetchProductInfo.bind(this);
+    this.fetchStyleInfo = this.fetchStyleInfo.bind(this);
+    this.fetchAllInfo = this.fetchAllInfo.bind(this);
   }
-
   fetchProductInfo(id) {
     axios.get(`http://3.21.164.220/products/${id}`)
       .then(data => this.setState({productInfo: data.data}))
       .catch(/*console.error('error at fetch product info')*/);
   }
-
+  fetchStyleInfo(id) {
+    axios.get(`http://3.21.164.220/products/${id}/styles`)
+      .then(data => this.setState({styleInfo: data.data.results[0].photos}))
+      .catch(/*console.error('error at fetch product info')*/);
+  }
+  fetchAllInfo(id) {
+    this.fetchStyleInfo(id);
+    this.fetchProductInfo(id);
+  }
   componentDidUpdate(prevProps) {
     if (prevProps.productId !== this.props.productId) {
-      this.fetchProductInfo(this.props.productId);
+      this.fetchAllInfo(this.props.productId);
     }
   }
-
   componentDidMount() {
-    this.fetchProductInfo(this.props.productId);
+    this.fetchAllInfo(this.props.productId);
   }
 
   render() {
     return (
-      <div
-        calssname="productCardContainer"
-        onClick={() => this.props.productChangeMethod(this.state.productInfo)}>
-        Product {this.state.productInfo.id}
-      </div>
+      <Card
+        onClick={()=>this.props.productChangeMethod(this.state.productInfo)} >
+        <Card.Body>
+          <Card.Img varient="top" src="https://via.placeholder.com/400/700" />
+          <Card.Text class="test">{this.state.productInfo.category}</Card.Text>
+          <Card.Title>{this.state.productInfo.name}</Card.Title>
+          <Card.Text>${this.state.productInfo.default_price}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
