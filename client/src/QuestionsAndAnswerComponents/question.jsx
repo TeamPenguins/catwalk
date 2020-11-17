cd855ffceac42812875bf1cd9b8b3650be9de7a2
Jonathan Garcia <jagspurs7@gmail.com>
2020-11-16 19:58:00 -0600

    got the question/answer list to dynamically render a list of question with matching answers using the selected product ID and making the proper API calls (one for list of Q's and one for list of A's)

--- client/src/QuestionsAndAnswerComponents/question.jsx
+++ client/src/QuestionsAndAnswerComponents/question.jsx
@@ -1,12 +1,52 @@
 import React, {Component} from 'react';
-import { questionsList, answerList } from '../dummyData.js';
+import Answers from './answers.jsx';
 
-const Question = () => {
+class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      productId: this.props.productId,
+    };
+    this.fetchQuestionList = this.fetchQuestionList.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
 
-  return (
+  fetchQuestionList(prodId) {
 
-    <div></div>
+    fetch(`http://3.21.164.220/qa/questions/?product_id=${prodId}&count=4`)
+      .then(response => response.json())
+      .then((questionList) => {
+        this.setState({
+          questions: questionList.results,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-  );
+  componentDidMount() {
+    this.fetchQuestionList(this.state.productId);
+  }
 
-};
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.questions.map((singleQuestion) => {
+          return (
+            <div>Q: {singleQuestion.question_body}
+              <Answers questionId = {singleQuestion.question_id}/>
+            </div>
+          );
+        })}</div>
+
+      </div>
+    );
+  }
+}
+
+
+
+export default Question;
