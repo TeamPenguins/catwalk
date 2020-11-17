219b4133711fcb4fdc2b1065dc6b5c712e367e3e
Stephanie Green <steph.green44@gmail.com>
2020-11-16 18:22:36 -0700

    remove unused button import

--- client/src/OverviewComponents/ProductInfo.jsx
+++ client/src/OverviewComponents/ProductInfo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col } from 'react-bootstrap';
 
 class ProductInfo extends React.Component {
   constructor() {
