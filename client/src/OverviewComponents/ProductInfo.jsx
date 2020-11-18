967e18c919f9d58b94488f2e518ea4e83b350053
Stephanie Green <steph.green44@gmail.com>
2020-11-17 19:03:57 -0700

    adjust padding & margin

--- client/src/OverviewComponents/ProductInfo.jsx
+++ client/src/OverviewComponents/ProductInfo.jsx
@@ -9,17 +9,17 @@ class ProductInfo extends React.Component {
 
   render() {
     return (
-      <Row className="my-4">
+      <Row className="my-4 pb-5">
         <Col sm={8} className="border-right border-dark product-overview">
           <h3>{this.props.selectedProduct.slogan}</h3>
           <p>{this.props.selectedProduct.description}</p>
         </Col>
         <Col sm={4} className="product-features-list">
           <ul className="list-group list-group-flush">
-            <li className="list-group-item border-0"><Check className="pr-1" size={32} />GMO and pesticide free</li>
-            <li className="list-group-item border-0"><Check className="pr-1" size={32} />Made with 100% Genetic Modification</li>
-            <li className="list-group-item border-0"><Check className="pr-1" size={32} />This is made up</li>
-            <li className="list-group-item border-0"><Check className="pr-1" size={32} />It doesn't matter</li>
+            <li className="list-group-item border-0 mb-n3"><Check className="pr-1" size={32} />GMO and pesticide free</li>
+            <li className="list-group-item border-0 mb-n3"><Check className="pr-1" size={32} />Made with 100% Genetic Modification</li>
+            <li className="list-group-item border-0 mb-n3"><Check className="pr-1" size={32} />This is made up</li>
+            <li className="list-group-item border-0 mb-n3"><Check className="pr-1" size={32} />It doesn't matter</li>
           </ul>
         </Col>
       </Row>
