4236ba555ed8ba047dac3c6cf8c09df9baf55663
Stephanie Green <steph.green44@gmail.com>
2020-11-19 14:31:21 -0700

    adds dynamic updating of sizes and quantities available

--- client/src/OverviewComponents/Variants.jsx
+++ client/src/OverviewComponents/Variants.jsx
@@ -6,24 +6,24 @@ import { Star } from 'react-bootstrap-icons';
 
 const Variants = (props) => {
 
-  // const updateStylesAvail = () => {
-  var selection = props.selectedStyle || 1;
-  console.log('selection: ' + selection);
-
+  // selection is the style_id number of the image that is clicked on
+  var selection = props.selectedStyle || 1; // defaults to the first style of the camo onesie product
+  // the stylesAvail are the possible style options of the selected product
   var stylesAvail = props.styles.results;
-  var styleClicked = {};
-  console.log(stylesAvail);
+  // the styleClicked is the object of all possible skus
+  var styleClicked = props.styles.results[0]; // defaults to the first style of the product displayed
+
+  // compares the currently selected style id to the available styles and updates styleClicked to only contain the data for the style selected
   for (var i = 0; i < stylesAvail.length; i++) {
     if (stylesAvail[i].style_id === Number(selection)) {
       console.log(stylesAvail[i]);
       styleClicked = stylesAvail[i];
     }
   }
-  console.log('styleClicked: ' + styleClicked);
-  // };
 
+  // skus are the nested objects that contain the size and quantity available for the selected style
+  var skus = styleClicked.skus;
 
-  var skus = props.styles.results[0].skus;
   // creates arrays for the size and quantities from the selected style skus
   var sizeArr = [];
   var quantityArr = [];
