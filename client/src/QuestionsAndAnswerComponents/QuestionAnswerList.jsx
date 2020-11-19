2d436a856c3f688151672008a46338e6cfacadb6
Jonathan Garcia <jagspurs7@gmail.com>
2020-11-19 10:46:17 -0600

    added static search bar and buttons for demo purpose

--- client/src/QuestionsAndAnswerComponents/QuestionAnswerList.jsx
+++ client/src/QuestionsAndAnswerComponents/QuestionAnswerList.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Question from './question.jsx';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Form, Button } from 'react-bootstrap';
 
 class QuestionAnswerList extends Component {
   constructor(props) {
@@ -12,8 +12,16 @@ class QuestionAnswerList extends Component {
       <Container>
         <Card>
           <Card.Body>
-            <Card.Title >Questions and Answers</Card.Title>
+            <p>QUESTIONS {'&'} ANSWERS</p>
+            <Form>
+              <Form.Group>
+                <Form.Control type='text' placeholder = 'HAVE A QUESTION? SEARCH FOR AN ANSWER...'/>
+              </Form.Group>
+            </Form>
             <Question productId = {this.props.selectedProduct.id}/>
+            <p><Card.Link><small>LOAD MORE ANSWERS</small></Card.Link></p>
+            <Button variant="outline-secondary"><strong> MORE ANSWERED QUESTIONS </strong></Button> {' '}
+            <Button variant="outline-secondary"><strong> ADD A QUESTION + </strong></Button>
           </Card.Body>
         </Card>
       </Container>
