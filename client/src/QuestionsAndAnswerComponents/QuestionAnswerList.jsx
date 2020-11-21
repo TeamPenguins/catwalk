0c3fa312ba66a84a2332581acc11f5a5fdc91f8f
Jonathan Garcia <jagspurs7@gmail.com>
2020-11-20 18:06:43 -0600

    added functionality to 'More Answered Question' button. also refactor fetch method to pull a larger batch and only show two questions by default with an additonal each time the user clicks the button

--- client/src/QuestionsAndAnswerComponents/QuestionAnswerList.jsx
+++ client/src/QuestionsAndAnswerComponents/QuestionAnswerList.jsx
@@ -5,7 +5,28 @@ import { Card, Container, Form, Button } from 'react-bootstrap';
 class QuestionAnswerList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      moreQuestions: 2,
+    };
+    this.moreQuestionsClickHandler = this.moreQuestionsClickHandler.bind(this);
   }
+
+  moreQuestionsClickHandler() {
+    var newCount = this.state.moreQuestions + 2;
+    this.setState({
+      moreQuestions: newCount,
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      this.setState ({
+        moreQuestions: 2,
+      });
+    }
+  }
+
 
   render() {
     return (
@@ -18,9 +39,8 @@ class QuestionAnswerList extends Component {
                 <Form.Control type='text' placeholder = 'HAVE A QUESTION? SEARCH FOR AN ANSWER...'/>
               </Form.Group>
             </Form>
-            <Question productId = {this.props.selectedProduct.id}/>
-            <p><Card.Link><small>LOAD MORE ANSWERS</small></Card.Link></p>
-            <Button variant="outline-secondary"><strong> MORE ANSWERED QUESTIONS </strong></Button> {' '}
+            <Question productId = {this.props.selectedProduct.id} moreQuestions ={this.state.moreQuestions} key={this.props.selectedProduct.id}/>
+            <Button type ='button' variant="outline-secondary" onClick={this.moreQuestionsClickHandler}><strong> MORE ANSWERED QUESTIONS </strong></Button> {' '}
             <Button variant="outline-secondary"><strong> ADD A QUESTION + </strong></Button>
           </Card.Body>
         </Card>
